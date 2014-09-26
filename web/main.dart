6d25a85bbd065eb1d541af4d18b4502367887600
Bob Nystrom <robert@stuffwithstuff.com>
2014-09-26 16:17:21 -0700

    Web sandbox for testing.

---
+++ web/main.dart
@@ -0,0 +1,38 @@
+import 'dart:html';
+
+import 'package:dart_style/formatter.dart';
+
+TextAreaElement before;
+TextAreaElement after;
+
+void main() {
+  before = querySelector("#before") as TextAreaElement;
+  after = querySelector("#after") as TextAreaElement;
+
+  before.onKeyUp.listen((event) {
+    reformat();
+  });
+
+  reformat();
+}
+
+void reformat() {
+  var source = before.value;
+
+  try {
+    var formatter = new CodeFormatter();
+    after.value = formatter.format(CodeKind.STATEMENT, source).source;
+    return;
+  } on FormatterException catch(err) {
+    // Do nothing.
+  }
+
+  // Maybe it's a statement.
+  try {
+    var formatter = new CodeFormatter();
+    after.value = formatter.format(CodeKind.COMPILATION_UNIT, source).source;
+  } on FormatterException catch(err) {
+    // Maybe it's a statement.
+    after.value = "Format failed:\n$err";
+  }
+}
