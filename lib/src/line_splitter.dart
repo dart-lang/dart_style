cdbda25c62b304448942bfb27452f063dcc07638
Kevin Moore <github@j832.com>
2014-12-16 16:56:18 -0800

    fixed library name of line_splitter

--- lib/src/line_splitter.dart
+++ lib/src/line_splitter.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library dart_style.src.line_printer;
+library dart_style.src.line_splitter;
 
 import 'dart:math' as math;
 
