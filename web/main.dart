61ee682ef06845a1461e9cbaa6060b118360f731
Bob Nystrom <rnystrom@google.com>
2015-05-29 10:12:58 -0700

    Add page width slider to web demo app.

--- web/main.dart
+++ web/main.dart
@@ -5,11 +5,26 @@ import 'package:dart_style/dart_style.dart';
 TextAreaElement before;
 TextAreaElement after;
 
+int width = 80;
+
 void main() {
   before = querySelector("#before") as TextAreaElement;
   after = querySelector("#after") as TextAreaElement;
 
   before.onKeyUp.listen((event) {
+    reformat();
+  });
+
+  var columnMarker = querySelector(".column-marker");
+
+  var widthInput = querySelector("#width") as InputElement;
+  var widthOutput = querySelector("#width-output") as OutputElement;
+
+  widthInput.onInput.listen((event) {
+    widthOutput.value = widthInput.value;
+    width = int.parse(widthInput.value);
+    var pad = " " * width + "|";
+    columnMarker.innerHtml = "$pad $width columns" + "\n$pad" * 29;
     reformat();
   });
 
@@ -20,15 +35,15 @@ void reformat() {
   var source = before.value;
 
   try {
-    after.value = new DartFormatter().format(source);
+    after.value = new DartFormatter(pageWidth: width).format(source);
     return;
-  } on FormatterException catch (err) {
+  } on FormatterException {
     // Do nothing.
   }
 
   // Maybe it's a statement.
   try {
-    after.value = new DartFormatter().formatStatement(source);
+    after.value = new DartFormatter(pageWidth: width).formatStatement(source);
   } on FormatterException catch (err) {
     after.value = "Format failed:\n$err";
   }
