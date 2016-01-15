2ec1272337653c747838e8e5ed1ba02b516e918d
Bob Nystrom <rnystrom@google.com>
2016-01-14 17:27:44 -0800

    Bump to 0.2.3.
    R=nweiz@google.com
    
    Review URL: https://codereview.chromium.org//1593453002 .

--- bin/format.dart
+++ bin/format.dart
@@ -14,7 +14,7 @@ import 'package:dart_style/src/io.dart';
 import 'package:dart_style/src/source_code.dart';
 
 // Note: The following line of code is modified by tool/grind.dart.
-const version = "0.2.2";
+const version = "0.2.3";
 
 void main(List<String> args) {
   var parser = new ArgParser(allowTrailingOptions: true);
