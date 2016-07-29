f117187306467ac23d950aef9e43ecfebb3d3039
Bob Nystrom <rnystrom@google.com>
2016-07-28 17:50:50 -0700

    Bump to release 0.2.7.
    R=nweiz@google.com
    
    Review URL: https://codereview.chromium.org//2196513002 .

--- bin/format.dart
+++ bin/format.dart
@@ -14,7 +14,7 @@ import 'package:dart_style/src/io.dart';
 import 'package:dart_style/src/source_code.dart';
 
 // Note: The following line of code is modified by tool/grind.dart.
-const version = "0.2.4";
+const version = "0.2.7";
 
 void main(List<String> args) {
   var parser = new ArgParser(allowTrailingOptions: true);
