15115fad543f0ce480a8572db3272c9c712fe99e
Bob Nystrom <robert@stuffwithstuff.com>
2017-03-21 16:35:48 -0700

    Bump version to 1.0.0. (#605)
    Fix #587.

--- bin/format.dart
+++ bin/format.dart
@@ -14,7 +14,7 @@ import 'package:dart_style/src/io.dart';
 import 'package:dart_style/src/source_code.dart';
 
 // Note: The following line of code is modified by tool/grind.dart.
-const version = "0.2.16";
+const version = "1.0.0";
 
 void main(List<String> args) {
   var parser = new ArgParser(allowTrailingOptions: true);
