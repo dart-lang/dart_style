7aef9287732622ae605559ddca4843ded2ba86ea
Bob Nystrom <robert@stuffwithstuff.com>
2019-04-04 17:44:35 -0700

    Bump version to publish. (#801)

--- bin/format.dart
+++ bin/format.dart
@@ -15,7 +15,7 @@ import 'package:dart_style/src/source_code.dart';
 import 'package:dart_style/src/style_fix.dart';
 
 // Note: The following line of code is modified by tool/grind.dart.
-const version = "1.2.5";
+const version = "1.2.6";
 
 void main(List<String> args) {
   var parser = ArgParser(allowTrailingOptions: true);
