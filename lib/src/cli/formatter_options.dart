a99fa63a32dc541a99bf1acb177bee9bf71e536b
Robert Nystrom <rnystrom@google.com>
2020-04-22 17:03:30 -0700

    Bump to 1.3.5.

--- lib/src/cli/formatter_options.dart
+++ lib/src/cli/formatter_options.dart
@@ -13,7 +13,7 @@ import 'show.dart';
 import 'summary.dart';
 
 // Note: The following line of code is modified by tool/grind.dart.
-const dartStyleVersion = '1.3.4';
+const dartStyleVersion = '1.3.5';
 
 /// Global options that affect how the formatter produces and uses its outputs.
 class FormatterOptions {
