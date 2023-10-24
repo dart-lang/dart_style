a46c23f7be28306f8f7a60802e6ab83ecac78f15
Lasse R.H. Nielsen <lrn@google.com>
2023-10-24 10:03:11 +0200

    Update lib/src/debug.dart
    Keep as doc-comment, adding the then required `library;` to sit on.
    
    Co-authored-by: Nate Bosch <nbosch@google.com>

--- lib/src/debug.dart
+++ lib/src/debug.dart
@@ -2,7 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Internal debugging utilities.
+/// Internal debugging utilities.
+library;
+
 import 'dart:math' as math;
 
 import 'chunk.dart';
