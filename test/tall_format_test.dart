0249ea667e2787aaee7188adfa2d7a85f7c808f7
Robert Nystrom <rnystrom@google.com>
2024-05-20 11:23:05 -0700

    Remove unneeded library names.

--- test/tall_format_test.dart
+++ test/tall_format_test.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 @TestOn('vm')
-library dart_style.test.tall_format_test;
+library;
 
 import 'package:dart_style/dart_style.dart';
 import 'package:test/test.dart';
