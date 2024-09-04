b8a539af97da12e1aafc609bad8ba75f9d1ae5ee
Robert Nystrom <rnystrom@google.com>
2024-09-04 15:05:23 -0700

    Handle imports with "as" and "if" clauses in either order.
    PR #1550 fixed the bug where if you had both clauses, it would output
    them in the *wrong* order. But it turns out that the parser today allows
    them in *either* order even though the language spec doesn't.
    
    Since I've seen a handful of cases like this in the wild, instead of
    just failing to format, simply preserve the clause order that the user
    wrote.

--- lib/src/short/source_visitor.dart
+++ lib/src/short/source_visitor.dart
@@ -1955,6 +1955,19 @@ class SourceVisitor extends ThrowingAstVisitor {
       space();
       visit(node.uri);
 
+      // The language specifies that configurations must appear after any `as`
+      // clause but the parser incorrectly accepts them before it and code in
+      // the wild relies on that. Instead of failing with an "unexpected output"
+      // error, just preserve the order of the clauses if they are out of order.
+      // See: https://github.com/dart-lang/sdk/issues/56641
+      var wroteConfigurations = false;
+      if (node.asKeyword case var asKeyword?
+          when node.configurations.isNotEmpty &&
+              node.configurations.first.ifKeyword.offset < asKeyword.offset) {
+        _visitConfigurations(node.configurations);
+        wroteConfigurations = true;
+      }
+
       if (node.asKeyword != null) {
         soloSplit();
         token(node.deferredKeyword, after: space);
@@ -1963,7 +1976,10 @@ class SourceVisitor extends ThrowingAstVisitor {
         visit(node.prefix);
       }
 
-      _visitConfigurations(node.configurations);
+      if (!wroteConfigurations) {
+        _visitConfigurations(node.configurations);
+      }
+
       _visitCombinators(node.combinators);
     });
   }
