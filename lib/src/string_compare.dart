cab69766f4c0d47e6eabdc42cfe1dd58142dceac
Bob Nystrom <rnystrom@google.com>
2024-11-01 06:44:13 -0700

    Clean up README and TODO comments. (#1592)
    Clean up README and TODO comments.
    
    Updated the example in the README to one that uses the new style. I
    also took the opportunity to find a more interesting example and
    generally rewrote things based on how the formatter is used today.
    
    When we were in the middle of the rewrite, I used "TODO(tall)" and
    "TODO(perf)" comments to track things I wanted to look into before I
    considered the rewrite "done". Now that the rewrite is basically good
    enough, I went through those. If I think the TODO is no longer worth
    doing at all, I just deleted it. Otherwise, I turned it into a general
    "TODO(rnystrom)" for us to keep in mind at some unspecified time.

--- lib/src/string_compare.dart
+++ lib/src/string_compare.dart
@@ -2,12 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// TODO(tall): Now that the formatter may add and remove trailing commas and
-// reposition comments relative to `,`, `[`, `]`, `{`, and `}`, this function
-// is getting less and less precise. (For example, a bug in the formatter that
-// dropped all `[` tokens on the floor would still pass.) Consider a more
-// sophisticated approach for determining that the formatter preserved all of
-// the original code.
+// TODO(rnystrom): Now that the formatter may add and remove trailing commas
+// and reposition comments relative to `,`, `[`, `]`, `{`, and `}`, this
+// function is getting less and less precise. (For example, a bug in the
+// formatter that dropped all `[` tokens on the floor would still pass.)
+// Consider a more sophisticated approach for determining that the formatter
+// preserved all of the original code.
 
 /// Returns `true` if [c] represents a whitespace code unit allowed in Dart
 /// source code.
