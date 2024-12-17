2ce25ce60ef84710c8945263c20410bcd0396f73
Robert Nystrom <rnystrom@google.com>
2024-12-17 07:14:39 -0800

    Update `--version` number.

--- lib/src/cli/formatter_options.dart
+++ lib/src/cli/formatter_options.dart
@@ -12,7 +12,7 @@ import 'show.dart';
 import 'summary.dart';
 
 // Note: The following line of code is modified by tool/grind.dart.
-const dartStyleVersion = '3.0.0';
+const dartStyleVersion = '3.0.1';
 
 /// Global options that affect how the formatter produces and uses its outputs.
 final class FormatterOptions {
