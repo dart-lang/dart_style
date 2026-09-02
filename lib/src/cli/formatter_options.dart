18fc725a13d92ce2e5cc04062da472246e995bb2
Robert Nystrom <rnystrom@google.com>
2026-09-01 18:23:39 -0700

    Bump pubspec version.
    I forgot I had just published a version, so this PR with a new change
    needs to move to a wip version.

--- lib/src/cli/formatter_options.dart
+++ lib/src/cli/formatter_options.dart
@@ -13,7 +13,7 @@ import 'show.dart';
 import 'summary.dart';
 
 // Do not edit directly. Use tool/bump_version.dart.
-const dartStyleVersion = '3.1.13';
+const dartStyleVersion = '3.1.14';
 
 /// Global options parsed from the command line that affect how the formatter
 /// produces and uses its outputs.
